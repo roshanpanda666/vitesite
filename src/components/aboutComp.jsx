d4cf09719ddac832d7473e74fc7cc788cde84318
Roshan panda <roshanpanda666@gmail.com>
2023-06-20 21:35:20 +0530

    commiting

--- src/components/aboutComp.jsx
+++ src/components/aboutComp.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { Tilt } from 'react-tilt'
+
+const defaultOptions = {
+	reverse:        false,  // reverse the tilt direction
+	max:            35,     // max tilt rotation (degrees)
+	perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+	scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+	speed:          1000,   // Speed of the enter/exit transition
+	transition:     true,   // Set a transition on enter/exit.
+	axis:           null,   // What axis should be disabled. Can be X or Y.
+	reset:          true,    // If the tilt effect has to be reset on exit.
+	easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+}
 const AboutComp = () => {
   return (
     <>
@@ -31,10 +44,97 @@ const AboutComp = () => {
               </div>
             </div>
 
-            <div className=" w-[100%] lg:w-[50dvw] flex items-center lg:justify-center">
+            <div className=" w-[100%] lg:w-[70dvw] flex items-center lg:justify-center space-x-10 lg:flex-row flex-col lg:space-y-0 space-y-6">
+
 
             {/* copy the about section of sunil */}
             {/* with tilt npm package and yes make that responsive */}
+            <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+            <motion.div className="h-60 w-52 bg-gradient-to-b from-sky-400 to-sky-200 rounded-lg flex flex-col justify-center items-center hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]"
+            initial={{
+              x:100
+            }}
+            transition={{
+              duration:1
+            }}
+            whileInView={{
+              x:0
+            }}
+            >
+              <div className="text-4xl ">
+              <i class="fa-solid fa-computer"></i>
+              </div>
+
+              <div className="text-black font-bold text-2xl mt-10">
+                Web developer
+              </div>
+
+            </motion.div>
+            </Tilt>
+            <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+            <motion.div className="h-60 w-52 bg-gradient-to-b from-sky-400 to-sky-200 rounded-lg flex flex-col justify-center items-center hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]"
+            initial={{
+              x:100
+            }}
+            transition={{
+              delay:0.3,
+              duration:1
+            }}
+            whileInView={{
+              x:0
+            }}
+            >
+              <div className="text-3xl">
+              <i class="fa-solid fa-microchip"></i>
+              </div>
+              <div className="font-bold text-2xl mt-10">
+                Tech enthusiast
+              </div>
+            </motion.div>
+            </Tilt>
+            <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+            <motion.div className="h-60 w-52 bg-gradient-to-b from-sky-400 to-sky-200 rounded-lg flex flex-col justify-center items-center hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]"
+            initial={{
+              x:100
+            }}
+            transition={{
+              duration:1,
+              delay:0.5
+            }}
+            whileInView={{
+              x:0
+            }}
+            >
+              <div className="text-3xl">
+              <i class="fa-solid fa-pen"></i>
+              </div>
+              <div className="font-bold text-2xl mt-10">
+                Web Designer
+              </div>
+            </motion.div>
+            </Tilt>
+            <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+            <motion.div className="h-60 w-52 bg-gradient-to-b from-sky-400 to-sky-200 rounded-lg flex flex-col justify-center items-center hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]"
+            initial={{
+              x:100
+            }}
+            transition={{
+              duration:1,
+              delay:0.7
+            }}
+            whileInView={{
+              x:0
+            }}
+            >
+              <div className="text-3xl">
+              <i class="fa-solid fa-user"></i>
+              </div>
+              <div className="font-bold text-2xl mt-10">
+                Learner
+              </div>
+
+            </motion.div>
+            </Tilt>
             </div>
           </div>
 
